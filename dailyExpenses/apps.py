cb9a5d6d0b4683692f957623ab30d2e4a4ef6b02
KruzhokProg <Aa1@3$5^7>
2022-03-03 17:13:57 +0300

    djoser auth

--- dailyExpenses/apps.py
+++ dailyExpenses/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class DailyexpensesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dailyExpenses'
+
+
+class AccountsConfig(AppConfig):
+    name = 'accounts'
+
+    def ready(self):
+        from accounts import signals
