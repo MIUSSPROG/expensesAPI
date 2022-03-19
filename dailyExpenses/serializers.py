5a1f2fe0cf26e1f629c7f9ffd1873051f28a83ff
KruzhokProg <Aa1@3$5^7>
2022-03-19 13:53:36 +0300

    argon2 child

--- dailyExpenses/serializers.py
+++ dailyExpenses/serializers.py
@@ -90,7 +90,7 @@ class SaveChildEncodedSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         child = Child(
             login=validated_data['login'],
-            password=make_password(validated_data['password'], hasher='pbkdf2_sha256')
+            password=make_password(validated_data['password'], hasher='argon2')
         )
         child.save()
         return child
